b8462c2f68848100ed6e0069cac92fcb699f2fef
neginikhil <neginikhil021@gmail.com>
2025-10-23 09:25:41 +0530

    naming changes

--- src/main/java/com/codurance/training/tasks/TaskList.java
+++ src/main/java/com/codurance/training/tasks/TaskList.java
@@ -8,7 +8,7 @@ public final class TaskList {
     private final Projects projects = new Projects();
     private final Writer writer;
     private final IdGenerator idGenerator = new IdGenerator();
-    private final CommandFactory commandRegistry = new CommandFactory();
+    private final CommandFactory commandFactory = new CommandFactory();
 
     public TaskList(Writer writer) {
         this.writer = writer;
@@ -16,17 +16,17 @@ public final class TaskList {
     }
 
     private void registerCommands() {
-        commandRegistry.register("show", new ShowCommand(writer, projects));
-        commandRegistry.register("add", new AddCommand(projects, idGenerator));
-        commandRegistry.register("check", new CheckCommand(projects, true));
-        commandRegistry.register("uncheck", new CheckCommand(projects, false));
+        commandFactory.register("show", new ShowCommand(writer, projects));
+        commandFactory.register("add", new AddCommand(projects, idGenerator));
+        commandFactory.register("check", new CheckCommand(projects, true));
+        commandFactory.register("uncheck", new CheckCommand(projects, false));
     }
 
     public void execute(String commandLine) throws Exception {
         String[] commandRest = commandLine.split(" ", 2);
         String command = commandRest[0];
         String args = commandRest.length > 1 ? commandRest[1] : "";
-        commandRegistry.get(command).execute(args);
+        commandFactory.get(command).execute(args);
     }
     
 }
